44a067f181d1619985b3c3831cf27dc3cf21b8e0
webdmamun <73527253+webdmamun@users.noreply.github.com>
2021-10-30 21:31:28 +0600

    delete files

--- src/Pages/MyBookings/MyBookings.js
+++ src/Pages/MyBookings/MyBookings.js
@@ -1,33 +1,34 @@
-import { Container, Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./MyBookings.css";
-import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const MyBookings = () => {
+  const [services, setServices] = useState([]);
+  const [hotels, setHotels] = useState([]);
+
+  // load booked services
+  useEffect(() => {
+    fetch("http://localhost:5000/bookedservice")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  // load booked hotels
+  useEffect(() => {
+    fetch("http://localhost:5000/bookedhotel")
+      .then((res) => res.json())
+      .then((data) => setHotels(data));
+  }, []);
+
   return (
     <div>
       <Container>
-        <Nav.Link
-          as={NavLink}
-          to="/bookedhotel"
-          className="nav-item"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "#419DE2",
-          }}
-        >
-          Booked Hotel
-        </Nav.Link>
-        <Nav.Link
-          as={NavLink}
-          to="/bookedservice"
-          className="nav-item"
-          activeStyle={{
-            fontWeight: "bold",
-            color: "#419DE2",
-          }}
-        >
-          Booked Services
-        </Nav.Link>
+        <h2>My booked confirm Services</h2>
+        <h3>{services.email}</h3>
+        <button>Delete Service</button>
+        <h2>My Booked confirm Hotels</h2>
+        <h3>{hotels.email}</h3>
+        <button>Delete Hotel</button>
       </Container>
     </div>
   );
