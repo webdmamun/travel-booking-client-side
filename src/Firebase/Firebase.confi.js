922b3e953d8f662e9c2000c44679c6856ad75c0a
webdmamun <73527253+webdmamun@users.noreply.github.com>
2021-10-31 16:48:54 +0600

    env setup

--- src/Firebase/Firebase.confi.js
+++ src/Firebase/Firebase.confi.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCmLfLuvtFUtHMvZVVMZcjNkX39OmiuJms",
-  authDomain: "toor-1a2b9.firebaseapp.com",
-  projectId: "toor-1a2b9",
-  storageBucket: "toor-1a2b9.appspot.com",
-  messagingSenderId: "623514364123",
-  appId: "1:623514364123:web:dbcdc0c6581f99048084fe",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 export default firebaseConfig;
